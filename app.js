6deedd91d217373caa494bc4f744a63eeb16579d
Ankit <ankit1999roy@gmail.com>
2023-05-31 23:24:57 +0530

    new

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
 const express = require("express");
+var cons = require('consolidate');
 const path = require("path");
 const app = express();
-//const mongoose = require('mongoose');
-//const bodyparser =require("body-parser")
-//mongoose.connect('mongodb://localhost/Uemdatabase', {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Uemdatabase', {useNewUrlParser: true, useUnifiedTopology: true});
 const port = process.env.PORT || 8000;
 
-/*const uemschema = new mongoose.Schema({
+const uemschema = new mongoose.Schema({
     name: String,
     phone: String,
     email: String,
     address: String,
     desc: String,
-  });*/
+});
 
-//const uem = mongoose.model('uem', uemschema);
+const uem = mongoose.model('uem', uemschema);
 
-// express specific stuff
+
 app.use('/static' , express.static('static'))
 app.use(express.urlencoded())
 
-// pug specific stuff
-app.set('view engine'  ,'pug')// set the template engine as pug
-app.set('views ' , path.join(__dirname , 'views')) // set the view directory..
 
-//end points
+app.engine('html', cons.swig)
+app.set('views ' , path.join(__dirname , 'views')) 
+app.set('view engine'  ,'html')
+
+
+
 
 app.get('/' , (req ,res)=>{
-    const params = {}
-    res.status(200).render('home.pug' , params)
+    res.status(200).render('home.html' )
 })
 app.get('/contact' , (req ,res)=>{
-    const params = {}
-    res.status(200).render('contact.pug' , params)
+  
+    res.status(200).render('contact.html' )
 })
-app.get('/About' , (req ,res)=>{
-    const params = {}
-    res.status(200).render('About.pug' , params)
+app.get('/about' , (req ,res)=>{
+    
+    res.status(200).render('about.html')
 })
 
 
 
-/*app.post('/contact' , (req ,res)=>{
+app.post('/contact' , (req ,res)=>{
     var mydata = new uem(req.body);
     mydata.save().then(()=>{
         res.send(" saved into the database ..... Thanks for filling")
@@ -50,11 +51,8 @@ app.get('/About' , (req ,res)=>{
         res.status(400).send("Not inserted into the database")
     });
     //res.status(200).render('contact.pug' )
-})*/
+})
 
-
-
-// start the server
 
 app.listen(port, ()=> {
     console.log(`the application started succesfully at port ${port}`);
